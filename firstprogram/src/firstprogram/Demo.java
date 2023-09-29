b84f8225f9e48a4894a4accf211cc68f07e72c0c
R.R.28.7.23 <R.R.28.7.23@DESKTOP-99FGHDP.Dlink>
2023-09-29 10:47:48 +0530

    first commit

---
+++ firstprogram/src/firstprogram/Demo.java
@@ -0,0 +1,10 @@
+package firstprogram;
+
+public class Demo {
+
+	  public static void main(String[] args) {
+		  System.out.println("hello world");
+		  System.out.println("10");
+		  System.out.println("10 hello");
+		  }
+		 }
